f2a1749c7eb8b703ba1af0804595cec180ea26b2
Myriam-Mabrouki <116686541+Myriam-Mabrouki@users.noreply.github.com>
2024-01-22 18:03:56 +0100

    getmutex fait

---
+++ TP1/mpi_server.c
@@ -0,0 +1,16 @@
+#include "mpi_server.h"
+static server the_server;
+
+//ICI CODE THREAD SERVEUR A COMPLETER
+
+void start_server(void (*callback)(int tag, int source)){
+//A COMPLETER
+}
+
+void destroy_server(){
+//A COMPLETER
+}
+
+pthread_mutex_t* getMutex(){
+    return &the_server.mutex;
+}
