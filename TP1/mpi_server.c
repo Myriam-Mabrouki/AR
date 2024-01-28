4f1bc7242706e718fea1ff1d33a80b666ff18446
Myriam Mabrouki <myriam.mabrouki@tutanota.com>
2024-01-28 18:23:46 +0100

    pour l'instant je suis perdue

--- TP1/mpi_server.c
+++ TP1/mpi_server.c
@@ -1,14 +1,18 @@
 #include "mpi_server.h"
 static server the_server;
 
+void callback(int tag, int source){
+    
+}
+
 //ICI CODE THREAD SERVEUR A COMPLETER
 
 void start_server(void (*callback)(int tag, int source)){
-//A COMPLETER
+    the_server.callback=callback(tag,source);
 }
 
 void destroy_server(){
-//A COMPLETER
+
 }
 
 pthread_mutex_t* getMutex(){
